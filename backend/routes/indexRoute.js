1bdd291fa0479a7b30e46a298e250bc50860fba3
Devoun Edwards <THREExPENNYco@gmail.com>
2020-03-15 01:14:49 -0700

    adjustments to sendgrid

--- backend/routes/indexRoute.js
+++ backend/routes/indexRoute.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 // Index route
 router.route('/').get((req, res) => {
   console.log(req.headers.connection)
+  res.send("hello"); 
 })
 
 module.exports = router
