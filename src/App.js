ef3e03ec8e440d5390244599fc233793c5e82444
Devoun Edwards <THREExPENNYco@gmail.com>
2020-04-23 00:53:51 -0700

    importign styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component }  from 'react'
 import Navbar from './components/navbar.js'
-import Main from './components/main.js'
 import Styles from './Styles.css'
 // Main component for the app
 class App extends Component {
   render () {
     return (
       <section className="main">
-        <Main />
         <Navbar />  
       </section> 
     )
