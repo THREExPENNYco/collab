b498826baf261f89dd9596d37d8ba64b93f5f086
Devoun Edwards <THREExPENNYco@gmail.com>
2020-04-23 17:28:20 -0700

    local commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component }  from 'react'
 import Navbar from './components/navbar.js'
 import Styles from './Styles.css'
+import Hero from './components/hero.js'
 // Main component for the app
 class App extends Component {
   render () {
     return (
       <section className="main">
         <Navbar />  
+        <Hero />
       </section> 
     )
   }
---
+++ src/components/hero.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+function Hero () { 
+    <h6 className="hero">
+        PEER PRESSURE
+    </h6>
+}
+
+export default Hero
