c491e96caaea3b8e401ef45565eb6d15f018f5bd
Devoun Edwards <THREExPENNYco@gmail.com>
2020-04-30 16:58:34 -0700

    more dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,7 +4,6 @@ import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 
 function Dashboard (props) {
-  console.log(props)
   const currUser = props.location.state.currUser
   const [currUserData, setCurrUserData] = useState('')
   const [error, setError] = useState('')
@@ -23,8 +22,28 @@ function Dashboard (props) {
   }, [])
   return (
     <section className="dashboard">
-      { currUser ? <p className="hero-dashboard">DASHBOARD</p>
-        : <Redirect to='/login' />}
+	<section className="dashboard-hero"> 
+		<h1>DASHBOARD</h1>
+	</section>
+	  <section className="dashboard-info"> 
+	  	<section className="dashboard-info__section">
+			<h1 className="dashboard-info__section-header">
+				GROUPS
+			</h1> 
+			<section className="dashboard-info__section-info"> 
+				List Of Groups
+			</section>
+		</section>
+		<section className="dashboard-info__section">
+			<h1 className="dashboard-info__section-header"> 
+				GOALS
+			</h1>
+			<section className="dashboard-info__section-info"> 
+				List Of Goals
+			</section>
+		</section>
+		</section>
+      { currUser ?  null : <Redirect to='/login' />  }
     </section>
   )
 }
