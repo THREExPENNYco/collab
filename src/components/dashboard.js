1ff1ec059f9d9bc3832ea7512a69a6747b65b590
Devoun Edwards <THREExPENNYco@gmail.com>
2020-04-30 20:10:03 -0700

    adding button and mapping for groups and goals

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,51 +1,75 @@
 /* eslint-disable react/prop-types */
-import React, { useState, useEffect } from 'react'
-import { Redirect } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
 
-function Dashboard (props) {
-  const currUser = props.location.state.currUser
-  const [currUserData, setCurrUserData] = useState('')
-  const [error, setError] = useState('')
+function Dashboard(props) {
+  const currUser = props.location.state.currUser;
+  const [currUserData, setCurrUserData] = useState("");
+  const [error, setError] = useState("");
   useEffect(() => {
     axios
-      .get(`http://localhost:3030/dashboard/${currUser}`, { withCredentials: true })
+      .get(`http://localhost:3030/dashboard/${currUser}`, {
+        withCredentials: true,
+      })
       .then((res) => {
         if (res.status === 200) {
-          const userData = res.data
-          setCurrUserData(userData)
+          const userData = res.data;
+          setCurrUserData(userData);
         }
       })
       .catch((err) => {
-        setError(err)
-      })
-  }, [])
+        setError(err);
+      });
+  }, []);
+
+  function Button(props) { 
+	return ( 
+		<button className="dashboard-info__section-info__button">CREATE GROUP</button>
+	)
+  }
+
+  console.log(currUserData.groups)
   return (
     <section className="dashboard">
-	<section className="dashboard-hero"> 
-		<h1>DASHBOARD</h1>
-	</section>
-	  <section className="dashboard-info"> 
-	  	<section className="dashboard-info__section">
-			<h1 className="dashboard-info__section-header">
-				GROUPS
-			</h1> 
-			<section className="dashboard-info__section-info"> 
-				List Of Groups
-			</section>
-		</section>
-		<section className="dashboard-info__section">
-			<h1 className="dashboard-info__section-header"> 
-				GOALS
-			</h1>
-			<section className="dashboard-info__section-info"> 
-				List Of Goals
-			</section>
-		</section>
-		</section>
-      { currUser ?  null : <Redirect to='/login' />  }
+      <section className="dashboard-hero">
+        <h1>DASHBOARD</h1>
+      </section>
+      <section className="dashboard-info">
+        <section className="dashboard-info__section">
+          <h1 className="dashboard-info__section-header">GROUPS</h1>
+          <section className="dashboard-info__section-info">
+            {currUserData.group === 1
+              ? currUserData.groups.map((index, group) => {
+                  <li
+                    key={index}
+                    classname="dashboard-info_section-info__content"
+                  >
+                    {group.name}
+                  </li>
+                })
+              : <Button />}
+          </section>
+        </section>
+        <section className="dashboard-info__section">
+          <h1 className="dashboard-info__section-header">GOALS</h1>
+          <section className="dashboard-info__section-info">
+              {currUserData.goals
+              ? currUserData.goals.map((index, goal) => {
+                  <li
+                    key={index}
+                    className="dashboard-info_section-info__content"
+                  >
+                    {goal.name}
+                  </li>
+                })
+              : <Button />}
+          </section>
+        </section>
+      </section>
+      {currUser ? null : <Redirect to="/login" />}
     </section>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
