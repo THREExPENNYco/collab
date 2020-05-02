ae29860a97ae0e413ad997e11489ce8b0736f465
Devoun Edwards <THREExPENNYco@gmail.com>
2020-05-01 22:35:06 -0700

    adding group dashboard component

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import axios from "axios";
-import { set } from "mongoose";
 
 function Dashboard(props) {
   const currUser = props.location.state.currUser;
@@ -57,8 +56,9 @@ function Dashboard(props) {
                       key={index}
                       className="dashboard-info__section-info__content-groups__item"
                     >
-                      {group.groupName}
+                      <a>{group.groupName}</a>
                     </li>
+					<hr className="dashboard-info__section-info__content-group__item__hr"></hr>
                   </section>
                 ))}
             <button
---
+++ src/components/groupDashboard.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; 
+import { Redirect } from "react-router-dom"; 
+
+function groupDashboard(props) { 
+    return(
+    <section className="dashboard">
+        <section className="dashboard-hero">
+            <h1>DASHBOARD</h1>
+        </section>
+    </section>
+    )
+}
+
+export default groupDashboard;
