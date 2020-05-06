7b9d998e40e3338901261b7ff7cef97093683ed6
Devoun Edwards <THREExPENNYco@gmail.com>
2020-05-05 22:55:16 -0700

    updates

--- src/components/groupDashboard.js
+++ src/components/groupDashboard.js
@@ -14,7 +14,7 @@ function groupDashboard(props) {
       .then((res) => {
         if (res.status === 200) {
           setGroup(res.data);
-          setGroupName(res.data.groupName)
+          setGroupName(res.data.groupName);
           getPeerMemberNames();
         }
       })
@@ -35,23 +35,27 @@ function groupDashboard(props) {
       });
   };
   return (
-    <section> 
+    <section>
       <p className="dashboard-hero__top">{groupName.toUpperCase()}</p>
       <p className="dashboard-hero__bottom">DASHBOARD</p>
-    <section className="dashboard-group">
-      <section className="dasboard-group__peers-section">
-        <section className="dashboard-group__members">
-          <h1 className="dashboard-group__members-header">PEERS</h1>
-          <hr className="dashboard-group__members-header__hr"></hr>
-          {groupPeers.map((member, index) => (
-            <p key={index} className="dashbaord-group__members-peers">
-              {member.userName}
-            </p>
-          ))}
+      <hr className="dashboard-hero__hr"></hr>
+      <section className="dashboard-group">
+        <section className="dasboard-group__peers-section">
+          <section className="dashboard-group__members">
+            <h1 className="dashboard-group__members-header">PEERS</h1>
+            <hr className="dashboard-group__members-header__hr"></hr>
+            {groupPeers.map((member, index) => (
+              <p key={index} className="dashbaord-group__members-peers">
+                {member.userName}
+              </p>
+            ))}
+          </section>
+        </section>
+        <section className="dashboard-group__members-goalsteps">
+          <h1 className="dashbaord-hero_top">FEED</h1>
         </section>
       </section>
     </section>
-        </section>
   );
 }
 
