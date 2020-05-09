afe414c3d3b523bc1b896cc86723118406675ce2
Devoun Edwards <THREExPENNYco@gmail.com>
2020-05-09 03:05:31 -0700

    dist

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -11,7 +11,7 @@ function LoginForm() {
   const login = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:3030/login", {
+      .post("https://salty-basin-04868.herokuapp.com//login", {
         userName: user,
         passWord: userPassWord,
       })
