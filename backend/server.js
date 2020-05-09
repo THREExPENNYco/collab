e186d2b42d2d389df20662f86f937105d7547695
Devoun Edwards <THREExPENNYco@gmail.com>
2020-05-09 01:42:55 -0700

    dist

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,8 @@ app.use(
     },
   })
 );
+
+console.log("This is the ACESS_TOKEN:", process.env.ACCESS_TOKEN);
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
   res.header("Access-Control-Request-Method", "POST, GET, OPTIONS");
