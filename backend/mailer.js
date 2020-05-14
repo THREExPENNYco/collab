ff3d79c39491a32b49203ba2e6d20be64ff0d0c3
Devoun Edwards <THREExPENNYco@gmail.com>
2020-05-13 21:37:38 -0700

    sendgrid configuration

--- backend/mailer.js
+++ backend/mailer.js
@@ -1,29 +1,30 @@
 require("dotenv").config({ path: "../.env" });
-const nodeMailer = require('nodemailer') 
+const nodeMailer = require("nodemailer");
+const sgTransport = require("nodemailer-sengrid-transport");
 
-const account = nodeMailer.createTransport({ 
-    service: "SendGrid",
-    auth: { 
-        user: "mukco", 
-        pass: process.env.MAILER_PASSWORD 
-    }
-}); 
+const options = {
+  service: "SendGrid",
+  auth: {
+    user: "mukco",
+    pass: process.env.MAILER_PASSWORD,
+  },
+};
+
+const account = nodeMailer.createTransport(sgTransport(options));
 
 const sendInviteEmail = (user) => {
-const emailSettings = { 
-    from: 'peerpressureappinfo@gmail.com', 
-    to: user, 
-    subject: 'You\'ve Been Invited', 
-    html: 
-        '<h1>You Have Been Invited</h1>'
-}; 
-account.sendMail(emailSettings, (err, info) => { 
-    if (err) { 
-        console.log(err)
+  const email = {
+    from: "peerpressureappinfo@gmail.com",
+    to: user,
+    subject: "You've Been Invited",
+    html: "<h1>You Have Been Invited</h1>",
+  };
+  account.sendMail(email, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
     }
-    else { 
-        console.log(info)
-    }
-}); 
-}
-module.exports = { sendInviteEmail }
+  });
+};
+module.exports = { sendInviteEmail };
