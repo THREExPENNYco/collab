fd04e9fa3e9f108c05618326aec4db94da19e141
Devoun Edwards <THREExPENNYco@gmail.com>
2020-05-17 16:51:46 -0700

    new dist

---
+++ backend/S3.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const AWS = require("aws-sdk");
+const S3 = require("aws-sdk/clients/s3");
+require("dotenv").config({ path: "../.env" });
+
+AWS.config = new AWS.Config();
+
+AWS.config.update({
+  accessKeyId: process.env.AMAZON_ACCESS_KEY,
+  secretAcesssKey: process.env.AMAZON_ACCESS_KEY,
+  region: "us-east-1",
+});
+
+let s3 = new AWS.S3({
+  accessKeyId: process.env.AMAZON_ACCESS_KEY,
+  secretAccessKey: process.env.AMAZON_SECRET_KEY,
+});
+
+const upLoadCommentImage = (file) => {
+  const image = file;
+  const config = {
+    Key: path.basename(image),
+    Bucket: "peerpressurebucket/commentPics",
+    Body: fs.createReadStream(image),
+  };
+  s3.upload(config, function (err, data) {
+    if (err) throw new err();
+    return data.Location;
+  });
+};
+
+module.exports = { upLoadCommentImage };
--- backend/userRoutes.js
+++ backend/userRoutes.js
@@ -7,6 +7,7 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const path = require("path");
 const { sendInviteEmail } = require("./mailer.js");
+const { uploadCommentImage } = require("./S3.js")
 
 // Root route for users
 router.route("/").get((req, res) => {
@@ -89,7 +90,7 @@ router.route("/dashboard/:userName").get((req, res) => {
 // route to create the comments
 router.route("/group_dashboard/group_id=:group_id/create_comment").post((req, res) => {
   const text = req.body.text;
-  const image = req.body.image;
+  const image = uploadCommentImage(req.body.image);
   const newComment = new Comment({
     createdBy: { 
       userId: req.session.userId,
