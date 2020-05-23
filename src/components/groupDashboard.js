375fe511b834212d2374effdaa44ab8bea973bf8
Devoun Edwards <THREExPENNYco@gmail.com>
2020-05-22 18:32:50 -0700

    new dist

--- src/components/groupDashboard.js
+++ src/components/groupDashboard.js
@@ -169,7 +169,8 @@ function groupDashboard(props) {
   const convertHtmlFile = (file) => {
     const reader = new FileReader();
     reader.onload = function () { 
-      const image = new Blob(file, { type: "image/*" });
+      console.log(reader.result)
+      const image = new Blob(reader.result, { type: "image/*" });
       setNewImage({newImageData: image, newImageUploaded: true});
     } 
     reader.readAsArrayBuffer(file);
