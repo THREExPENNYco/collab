0c17a3b26965acae3353e371121fc564e5ee1482
Devoun Edwards <THREExPENNYco@gmail.com>
2020-05-23 17:18:42 -0700

    new dist

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const path = require("path");
 const sessions = require("express-session");
 const FileStore = require("session-file-store")(sessions);
+const AWS = require("aws-sdk");
+const multer = require("multer"); 
+const multerS3 = require("multer-s3");
 
 require("dotenv").config({ path: "../.env" });
 
@@ -12,6 +15,29 @@ const port = process.env.PORT || 3030;
 
 app.use(cors());
 app.use(express.json());
+
+AWS.config = new AWS.Config();
+
+AWS.config.update({
+  accessKeyId: process.env.AMAZON_ACCESS_KEY,
+  secretAcesssKey: process.env.AMAZON_ACCESS_KEY,
+  region: "us-east-1",
+});
+
+let s3 = new AWS.S3({
+  accessKeyId: process.env.AMAZON_ACCESS_KEY,
+  secretAccessKey: process.env.AMAZON_SECRET_KEY
+});
+
+const upload = multer({ 
+  storage: multerS3({ 
+    s3: s3, 
+    bucket: "peerpressurebucket/commentPics", 
+    key: function (req, file, cb) { 
+      cb(null, Date.now().toString())
+    }
+  })
+})
 
 mongoose.set("useFindAndModify", false);
 const mongoUri = process.env.MONGODB_URI;
@@ -49,7 +75,9 @@ app.use(express.static("dist"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
-
+app.post("/group_dashboard/group_id=:group_id/create_comment", upload.single("comment_pic"), function(req, res, next) { 
+  next();
+})
 app.listen(port, () => {
   console.log(`You\'re listening on: ${port}`);
 });
