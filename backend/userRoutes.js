0bfbf9f91cac13c17a9f6ae1a36edacd614db1a3
Devoun Edwards <THREExPENNYco@gmail.com>
2020-05-23 17:24:11 -0700

    new dist

--- backend/userRoutes.js
+++ backend/userRoutes.js
@@ -89,8 +89,7 @@ router.route("/dashboard/:userName").get((req, res) => {
 // route to create the comments
 router.route("/group_dashboard/group_id=:group_id/create_comment").post((req, res) => {
   const text = req.body.text;
-  console.log(req.file)
-  const image = upLoadCommentImage(req.body.image, `${req.session.userId}-${Math.random()}`);
+  console.log(req.file);
   const newComment = new Comment({
     createdBy: { 
       userId: req.session.userId,
