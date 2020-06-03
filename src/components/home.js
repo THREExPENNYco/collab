c06d14601558abd6b63533c07ade654d0d4f14de
Devoun Edwards <THREExPENNYco@gmail.com>
2020-06-02 19:03:35 -0700

    new dist

--- src/components/home.js
+++ src/components/home.js
@@ -4,14 +4,11 @@ import Info from "./info.js";
 import { Redirect } from "react-router-dom";
 
 function Home() {
-  const [currUser, setCurrUser] = useState(false);
-  localStorage.getItem("currUser") ? setCurrUser(localStorage.getItem("currUser")) : setCurrUser(false);
-  console.log(localStorage.getItem("currUser"));
   return (
     <section className="main">
       <Hero />
       <Info />
-      { currUser ? <Redirect to={{ pathname: `/dashboard/${currUser}` }} /> : null }
+      { localStorage.getItem("currUser") ? <Redirect to={{ pathname: `/dashboard/${currUser}` }} /> : null }
     </section>
   );
 }
