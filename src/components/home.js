82c6aef24fd80a6d0d946982e93ea879f748509f
Devoun Edwards <THREExPENNYco@gmail.com>
2020-06-02 17:48:27 -0700

    redirect for home page if user loffed in

--- src/components/home.js
+++ src/components/home.js
@@ -4,21 +4,13 @@ import Info from "./info.js";
 import { Redirect } from "react-router-dom";
 
 function Home() {
-  {
-    const currUser = localStorage.getItem("currUser");
-    currUser ? (
-      <Redirect
-        to={{
-          pathname: `/dashboard/${currUser}`,
-        }}
-      />
-    ) : null;
-  }
-  console.log(localStorage.getItem("currUser"))
+  const currUser = localStorage.getItem("currUser");
+  console.log(localStorage.getItem("currUser"));
   return (
     <section className="main">
       <Hero />
       <Info />
+      { currUser ? <Redirect to={{ pathname: `/dashboard/${currUser}` }} /> : null }
     </section>
   );
 }
