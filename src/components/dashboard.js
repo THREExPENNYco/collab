559a33653ffbbaa9a11fd01a32b4d1b297d03f1f
Devoun Edwards <THREExPENNYco@gmail.com>
2020-06-08 23:48:40 -0700

    change to goals route

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -72,7 +72,7 @@ function Dashboard(props) {
   };
   const getCurrUserGoals = (e) => { 
     axios
-      .get(`https://salty-basin-04868.herokuapp.com/group_dashboard/:group_id/goals/currUser=${currUser}`)
+      .get(`https://salty-basin-04868.herokuapp.com/goals/currUser=${currUser}`)
       .then((res) => {
         if (res.status === 200) { 
           setCurrUserGoals(currUserGoals.concat(res.goals));
