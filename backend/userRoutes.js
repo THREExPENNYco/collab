8d6b2406f8aeb3521f52505b5dca2bd4812dd3f3
Devoun Edwards <THREExPENNYco@gmail.com>
2020-06-11 23:18:37 -0700

    change to res sent data

--- backend/userRoutes.js
+++ backend/userRoutes.js
@@ -105,10 +105,10 @@ router.route('/goals/curr_user?=:curr_user').get((req, res) => {
 router.route('/dashboard/curr_user?=:curr_user').get((req, res) => {
 	User.findOne({ userName: req.params.curr_user })
 		.then((user) => { 
-			// if (checkSesssionAndSessionId()) {
-			// 	res.status(401).json(err);
-			// 	return;
-			// } 
+			if (checkSesssionAndSessionId()) {
+				res.status(401).json(user);
+				return;
+			} 
 			res.status(200).json(user);
 		})
 		.catch((err) => { 
