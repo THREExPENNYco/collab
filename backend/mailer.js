4c53ad5886886eb0f29024013f3c907d201c3e2e
Devoun Edwards <THREExPENNYco@gmail.com>
2020-06-14 22:09:11 -0700

    change to concatenation

--- backend/mailer.js
+++ backend/mailer.js
@@ -11,9 +11,9 @@ const sendInviteEmail = (user, groupId) => {
 		subject: "You've been invited to a Peer Pressure group!",
 		text: 'You have been invited to the a Peer Pressure Group! Get ready to',
 		html:
-			`<strong>and easy to do anywhere, even with Node.js</strong> 
-			<button type="button" style="${emailButtonStyles}"> 
-			<a style="decoration: none; color: white;" href="${emailHref}>JOIN</a>
+			`<strong>and easy to do anywhere, even with Node.js</strong> +  
+			<button type="button" style="${emailButtonStyles}"> + 
+			<a style="decoration: none; color: white;" href="${emailHref}>JOIN</a> +
 			</button>`,
 	};
 	sgMail
