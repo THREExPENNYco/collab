62047fbc478b2255b2a27aacda137f4a52e4d5d6
Devoun Edwards <THREExPENNYco@gmail.com>
2020-06-18 22:07:11 -0700

    fix to CurrUserContext not being defined?

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
+import  { CurrUserContext } from '../App.js';
 
 function LoginForm() {
 	const { currUser, setCurrUser } = useContext(CurrUserContext);
