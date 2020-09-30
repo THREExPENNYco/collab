55fc89b9c24d3950b96e6baf1ddaf8383c78fca0
Devoun Edwards <THREExPENNYco@gmail.com>
2020-09-29 23:46:56 -0700

    Fixing compile error

--- backend/mailer.js
+++ backend/mailer.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.MAILER_PASSWORD.trim());
 const sendInviteEmail = (user, groupId) => {
 	const styles = { tagStyles: 'text-align: center; font-size: 12px;',
 	buttonStyles: 
-		'display: inherit; margin-top: 15px; margin-left: auto; margin-right: auto; color: white; background-color: black; border-style: none; width: 85px; height: 25px;'; } 
+		'display: inherit; margin-top: 15px; margin-left: auto; margin-right: auto; color: white; background-color: black; border-style: none; width: 85px; height: 25px;' } 
 	const emailHref = `https://salty-basin-04868.herokuapp.com/groups/group_id=${groupId}/add_user_to_group`;
 	const msg = {
 		to: user.toString().trim(),
